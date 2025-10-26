49a62b60218ff4303929258ce8b803107dfc7220
jafethBM <jafethbadillomartinez@gmail.com>
2025-10-26 02:04:05 -0600

    routesLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
   { path: '', component: CounterComponent },
   { path: 'hero', component: HeroPageComponent },
+  { path: 'dragonball', component: DragonballPageComponent},
+  { path: '**', redirectTo: '' },
 
 ];
